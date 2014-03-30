9278d84c50ab6f2e11c036591b228be004f1d882
glindauer <glindauer@att.net>
2014-03-30 19:58:51 -0400

    default_loc and new sysfunc

--- click.m
+++ click.m
@@ -6,45 +6,74 @@
 //
 // Usage:
 // ./click -x pixels -y pixels 
-// At the given coordinates it will click and release.
+// At the given coordinates it will click and release. If none are given, it will click at the current mouse location
 
 
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
 
+//Global mouse position as stored by us. This will differ from the real mouse position if the cursor is moved between calls to setMouseLoc
+CGPoint gPosMouse;
 
+void getMouseLoc(CGPoint *ptCurLoc)
+{
+	CGEventRef curLocEvent = CGEventCreate(NULL);
+	*ptCurLoc = CGEventGetLocation(curLocEvent);
+	gPosMouse=*ptCurLoc;
+	CFRelease(curLocEvent);
+}
+void setMouseLoc(CGPoint *ptNewLoc)
+{
+	CGEventRef mouseMoveEv = CGEventCreateMouseEvent (NULL,kCGEventMouseMoved,*ptNewLoc,0);
+	CGEventPost (kCGHIDEventTap, mouseMoveEv);
+	CFRelease(mouseMoveEv);
+	gPosMouse=*ptNewLoc;
+}
+void mouseLeftClick(CGPoint *pt)
+{
+	CGEventRef mouseDownEv = CGEventCreateMouseEvent (NULL,kCGEventLeftMouseDown,*pt,kCGMouseButtonLeft);
+	CGEventPost (kCGHIDEventTap, mouseDownEv);
+
+	CGEventRef mouseUpEv = CGEventCreateMouseEvent (NULL,kCGEventLeftMouseUp,*pt,kCGMouseButtonLeft);
+	CGEventPost (kCGHIDEventTap, mouseUpEv );
+
+  CFRelease (mouseDownEv);
+  CFRelease (mouseUpEv);
+}
 int main(int argc, char *argv[]) {
+  /* quartz requires a memory pool, hence the NSAutoreleasePool and
+   * [pool release] at the end
+   */
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
 
+  /* a minimal sleep is needed for the system to register the 
+   * key-up event for the return key the user presses to invoke this
+   * command from the command line
+   */
+  sleep (2);
 
   // grabs command line arguments -x and -y
   //
   int x = [args integerForKey:@"x"];
   int y = [args integerForKey:@"y"];
 
+  if (x==0 || y==0)
+  {
+	// user didn't enter one or both coordinates,
+	// meaning that we should click at the current coordinate
+
+	getMouseLoc(&ourLoc);
+	x = ourLoc.x;
+	y = ourLoc.y;
+  }
 
   // The data structure CGPoint represents a point in a two-dimensional
   // coordinate system.  Here, X and Y distance from upper left, in pixels.
   //
-  CGPoint pt;
-  pt.x = x;
-  pt.y = y;
+  CGPoint pt={x,y};
 
-
-  // This is where the magic happens.  See CGRemoteOperation.h for details.
-  //
-  // CGPostMouseEvent( CGPoint        mouseCursorPosition,
-  //                   boolean_t      updateMouseCursorPosition,
-  //                   CGButtonCount  buttonCount,
-  //                   boolean_t      mouseButtonDown, ... )
-  //
-  // So, we feed coordinates to CGPostMouseEvent, put the mouse there,
-  // then click and release.
-  //
-  CGPostMouseEvent( pt, 1, 1, 1 );
-  CGPostMouseEvent( pt, 1, 1, 0 );
-
+  mouseLeftClick(&pt);
 
   [pool release];
   return 0;
